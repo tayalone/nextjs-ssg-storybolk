e8dc1ec2b314b34d9e6828aecf70877d38916125
estellar_boyz <tayalone@gmail.com>
2020-06-24 21:08:10 +0700

    :sparkles: Add Storyblok bridge to layout

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from '../components/head'
 import Nav from '../components/nav'
+import StoryblokService from '../utils/storyblok-service'
 
 export default function layout({ children, settings = {} }) {
   return (
@@ -61,6 +62,7 @@ export default function layout({ children, settings = {} }) {
           height: 2px;
         }
       `}</style>
+      {StoryblokService.bridge()}
     </div>
   )
 }
